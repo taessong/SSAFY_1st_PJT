e15deae6d2e8580184b1701d70e2f773fe80b731
kts <tskim4065@gmail.com>
2024-05-22 15:04:32 +0900

    매칭게시판 수정

--- front/vue-PJT/src/components/match/MatchRegist.vue
+++ front/vue-PJT/src/components/match/MatchRegist.vue
@@ -1,88 +1,241 @@
 <template>
-    <div>
-      <h2>매치 등록 페이지</h2>
-      <fieldset>
-        <legend>매치 정보 입력</legend>
-  
-        <div>
-          <label for="content">매치 내용 : </label>
-          <textarea id="content" v-model="match.content"></textarea>
-        </div>
-        <div>
-          <label for="matchDate">경기 날짜 선택 : </label>
-          <input type="date" id="matchDate" v-model="match.matchDate">
-        </div>
-        <div>
-          <label for="matchTime">경기 시간 입력 : </label>
-          <input type="time" id="matchTime" v-model="match.matchTime">
-        </div>
-        <div>
-          <label for="stadium">경기장 선택 : </label>
-          <select id="stadium" v-model="selectedStadium" @change="updateStadiumImage">
-            <option v-for="stadium in stadiums" :key="stadium.stadium_id" :value="stadium.stadium_id">
-              {{ stadium.stadium_name }}
-            </option>
-          </select>
-          <div v-if="selectedStadium !== null">
-            <img :src="stadiumImage" alt="Stadium Image">
+  <div>
+    <h2>매치 등록 페이지</h2>
+    <fieldset>
+      <legend>매치 정보 입력</legend>
+      <div>
+        <label for="content">매치 내용 : </label>
+        <textarea id="content" v-model="match.content"></textarea>
+      </div>
+      <div>
+        <label for="matchDate">경기 날짜 선택 : </label>
+        <input type="date" id="matchDate" v-model="match.matchDate">
+      </div>
+      <div>
+        <label for="matchTime">경기 시간 입력 : </label>
+        <input type="time" id="matchTime" v-model="match.matchTime">
+      </div>
+      <div>
+        <label for="stadium">경기장 선택 : </label>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div id="carouselExampleCaptions1" class="carousel slide">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="@/asset/stadium/가오풋살장/가오풋살장(1).png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>가오풋살장</h5>
+                      <p>대전 동구 가오동 7-9</p>
+                      <button @click="selectStadium('가오풋살장')">Select</button>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="@/asset/stadium/가오풋살장/가오풋살장(2).png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>가오풋살장</h5>
+                      <p>대전 동구 가오동 7-9</p>
+                      <button @click="selectStadium('가오풋살장')">Select</button>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions1" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div id="carouselExampleCaptions2" class="carousel slide">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="@/asset/stadium/남선공원풋살장/남선공원풋살장(1).png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>남선공원풋살장</h5>
+                      <p>대전 서구 남선로 66</p>
+                      <button @click="selectStadium('남선공원풋살장')">Select</button>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="@/asset/stadium/남선공원풋살장/남선공원풋살장(2).png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>남선공원풋살장</h5>
+                      <p>대전 서구 남선로 66</p>
+                      <button @click="selectStadium('남선공원풋살장')">Select</button>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div id="carouselExampleCaptions3" class="carousel slide">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="@/asset/stadium/대전가장풋살존/대전가장풋살존(1).png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>대전가장풋살존</h5>
+                      <p>대전 서구 괴정로198번길 43 21-3</p>
+                      <button @click="selectStadium('대전가장풋살존')">Select</button>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="@/asset/stadium/대전가장풋살존/대전가장풋살존(2).png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>대전가장풋살존</h5>
+                      <p>대전 서구 괴정로198번길 43 21-3</p>
+                      <button @click="selectStadium('대전가장풋살존')">Select</button>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div id="carouselExampleCaptions4" class="carousel slide">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="@/asset/stadium/유성풋살구장/유성풋살구장(1).png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>유성풋살구장</h5>
+                      <p>대전 유성구 유성대로713번길 83</p>
+                      <button @click="selectStadium('유성풋살구장')">Select</button>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="@/asset/stadium/유성풋살구장/유성풋살구장(2).png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>유성풋살구장</h5>
+                      <p>대전 유성구 유성대로713번길 83</p>
+                      <button @click="selectStadium('유성풋살구장')">Select</button>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-        <div>
-          <button @click="createMatch">등록하기</button>
+      </div>
+      <div>
+        <div v-if="selectedStadiumName">
+          <p>선택된 경기장 : {{ selectedStadiumName }}</p>
+          <p>주소 : {{ selectedStadiumAddress }}</p>
         </div>
-      </fieldset>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref, reactive, onMounted } from 'vue';
-  import { useMatchStore } from '@/stores/match';
-  
-  const store = useMatchStore();
-  
-  const match = reactive({
-      authorName: sessionStorage.getItem("nickName"),
-      teamAName: sessionStorage.getItem("favoriteTeam"),
-      content: '',
-      matchDate: '',
-      matchTime: '',
-      stadiumId: ''
-  });
-  
-  const selectedStadium = ref(null);
-  const stadiums = ref([]);
-  const stadiumImage = ref('');
-  
-  const fetchStadiums = async () => {
-    // API를 통해 경기장 목록 가져오기
-    // 예시로 하드코딩한 경기장 목록 사용
-    stadiums.value = [
-      { stadium_id: 1, stadium_name: '유성풋살구장', stadium_img: '@/asset/stadium/유성풋살구장/유성풋살구장(1).png' },
-      { stadium_id: 2, stadium_name: '삼정풋살파크', stadium_img: '@/asset/stadium/삼정풋살파크/삼정풋살파크(1).png' },
-      { stadium_id: 3, stadium_name: '위너스풋살파크', stadium_img: '@/asset/stadium/위너스풋살파크/위너스풋살파크(1).png' },
-      { stadium_id: 4, stadium_name: '대전가장풋살존', stadium_img: '@/asset/stadium/대전가장풋살존/대전가장풋살존(1).png' },
-      { stadium_id: 5, stadium_name: '남선공원풋살장', stadium_img: '@/asset/stadium/남선공원풋살장/남선공원풋살장(1).png' },
-      { stadium_id: 6, stadium_name: '가오풋살장', stadium_img: '@/asset/stadium/가오풋살장/가오풋살장(1).png' }
-    ];
-    console.log(stadiums.value);
-  };
-  
-  const updateStadiumImage = () => {
-    // 선택한 경기장에 대한 이미지 업데이트
-    if (selectedStadium.value !== null) {
-      const stadium = stadiums.value.find(stadium => stadium.stadium_id === selectedStadium.value);
-      stadiumImage.value = stadium ? stadium.stadium_img : '';
+      </div>
+
+      <div>
+        <button @click="createMatch">등록하기</button>
+      </div>
+    </fieldset>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useMatchStore } from '@/stores/match';
+
+const store = useMatchStore();
+
+const match = ref({
+    authorName: sessionStorage.getItem("nickName"),
+    teamAName: sessionStorage.getItem("favoriteTeam"),
+    content: '',
+    matchDate: '',
+    matchTime: '',
+    stadiumId: 0
+});
+
+const selectedStadiumName = ref('');
+const selectedStadiumAddress = ref('');
+const selectedStadiumId = ref(0);
+
+// 경기장 이름을 선택했다? 그 경기장이 몇번째 인덱스인지 알아야 함.....
+const selectStadium = (stadiumName) => {
+  for(let i=0; i<store.stadiums.length; i++){
+    if(store.stadiums[i].name === stadiumName) {
+      selectedStadiumName.value = store.stadiums[i].name;
+      selectedStadiumAddress.value = store.stadiums[i].address;
+      selectedStadiumId.value = store.stadiums[i].id;
+      break;
     }
-  };
-  
-  const createMatch = () => {
-    match.stadiumId = selectedStadium.value;
-    store.createMatch(match);
-  };
-  
-  onMounted(fetchStadiums);
-  </script>
-  
-  <style scoped>
-  </style>
-  
+  }
+};
+
+const createMatch = () => {
+  console.log(match)
+  match.value.stadiumId = parseInt(selectedStadiumId.value);
+  store.createMatch(match);
+};
+
+</script>
+
+<style scoped>
+.carousel {
+  max-width: 100%;
+  max-height: 100%;
+  width: auto;
+  height: auto;
+}
+
+/* 캐러셀 내부 이미지 크기 조정 */
+.carousel-inner img {
+  max-width: 100%;
+  max-height: 100%;
+  width: auto;
+  height: auto;
+}
+
+.row {
+  padding: 20px;
+}
+
+.carousel-caption {
+  background: rgba(0, 0, 0, 0.5); /* 캡션의 배경을 반투명하게 설정 */
+  padding: 10px; /* 캡션의 패딩을 추가 */
+  border-radius: 5px; /* 캡션의 모서리를 둥글게 설정 */
+}
+</style>
--- front/vue-PJT/src/main.js
+++ front/vue-PJT/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const app = createApp(App)
 
@@ -32,3 +33,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.min.js"
