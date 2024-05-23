7b58f170ebb09b7ee12089fe7c50ab286de06f78
kts <tskim4065@gmail.com>
2024-05-23 11:23:59 +0900

    헤더바를 고쳐보자

--- front/vue-PJT/src/components/nav/HeaderBar.vue
+++ front/vue-PJT/src/components/nav/HeaderBar.vue
@@ -1,26 +1,32 @@
 <template>
-  <header class="header-bar" :style="headerStyle">
-    <RouterLink :to="{ name: 'login' }"><h1>RunOnGround</h1></RouterLink>
-    <div class="header-sub">
-      <RouterLink :to="{ name: 'clubBoard' }"><h3>커뮤니티</h3></RouterLink>
-      <RouterLink :to="{ name: 'recruitRegist' }"><h3>팀원모집</h3></RouterLink>
-      <RouterLink :to="{ name: 'matchboard' }"><h3>매칭</h3></RouterLink>
-      <a href="https://www.naver.com" class="a"><h3>QnA With GPT</h3></a>
-    </div>
-    <div class="header-login">
-      <div v-if="nickName" class="user-info">
-        <img :src="logoSrc" alt="favoriteTeam" v-if="logoSrc" class="logoImg">
-        <h3>{{ nickName }}님, 안녕하세요!</h3>
+  <header class="header-bar">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-2">
+          <RouterLink :to="{ name: 'login' }"><h4>⚽</h4></RouterLink>
+        </div>
+        <div class="col-md-2">
+          <RouterLink :to="{ name: 'clubBoard' }"><h4>📢</h4></RouterLink>
+        </div>
+        <div class="col-md-2">
+          <RouterLink :to="{ name: 'matchboard' }"><h4>👊🏼</h4></RouterLink>
+        </div>
+        <div class="col-md-4" v-if="nickName" style="display: flex; align-items: center;">
+          <img :src="logoSrc" alt="favoriteTeam" v-if="logoSrc" class="logoImg">
+          <h4 style="color: black;">{{ nickName }}님, 안녕하세요!</h4>
+        </div>
+        <div class="col-md-2">
+          <RouterLink v-if="nickName" :to="{ name: 'login' }" @click="handlerLogout"><h4>📴</h4></RouterLink>
+        </div>
       </div>
-      <RouterLink v-if="nickName" :to="{ name: 'login' }" @click="handlerLogout">
-        <h3>Log Out</h3>
-      </RouterLink>
+    </div>
+    <div class="team-img">
+      <img :src="teamImageSrc" alt="Team Image" ref="teamImage">
     </div>
   </header>
 </template>
-
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, nextTick } from "vue";
 import { useRouter } from 'vue-router';
 
 // 모든 팀의 배경 이미지를 사전에 import
@@ -43,30 +49,47 @@ const teamBackgrounds = {
 const nickName = ref(null);
 const logoSrc = ref('');
 const headerStyle = ref({ backgroundColor: 'black' }); // 기본 배경색 설정
+const teamImageSrc = ref('');
 const router = useRouter();
+const teamImage = ref(null);
 
 const updateNickName = () => {
   nickName.value = sessionStorage.getItem("nickName");
 }
 
-onMounted(() => {
+onMounted(async () => {
   updateNickName();
   const favoriteTeam = sessionStorage.getItem("favoriteTeam");
   if (favoriteTeam) {
     // 배경 이미지 설정
     if (teamBackgrounds[favoriteTeam]) {
-      headerStyle.value = { backgroundImage: `url(${teamBackgrounds[favoriteTeam]})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundColor: 'black' };
+      headerStyle.value.backgroundImage = `url(${teamBackgrounds[favoriteTeam]})`;
     }
     // 동적 import를 사용하여 프로필 이미지 설정
-    import(`../../asset/logo/${favoriteTeam}.png`)
-      .then(module => {
-        logoSrc.value = module.default;
-      })
-      .catch(error => {
-        console.error("Error loading logo:", error);
-      });
+    try {
+      const module = await import(`../../asset/logo/${favoriteTeam}.png`);
+      logoSrc.value = module.default;
+    } catch (error) {
+      console.error("Error loading logo:", error);
+    }
+    // 팀 이미지 설정
+    teamImageSrc.value = teamBackgrounds[favoriteTeam];
+    nextTick(() => {
+      adjustImageSize();
+    });
   }
 });
+
+const adjustImageSize = () => {
+  if (teamImage.value) {
+    const img = teamImage.value;
+    img.style.width = 'auto';
+    img.style.height = 'auto';
+    img.style.maxWidth = '100%';
+    img.style.maxHeight = '300px';
+    img.style.objectFit = 'contain';
+  }
+}
 
 window.addEventListener('storage', () => {
   updateNickName();
@@ -81,46 +104,57 @@ const handlerLogout = (event) => {
   router.go(0);
 };
 </script>
-
 <style scoped>
 .header-bar {
-  background-color: black;
-  padding: 1px;
+  padding: 10px 20px;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  width: 1903px;
-  height: 300px;
+}
+
+.container {
+  width: 100%;
+  max-width: 1200px;
+}
+
+.row {
+  width: 100%;
 }
 
 .header-sub {
   display: flex;
   margin-top: 15px;
   margin-right: auto;
-  align-items: flex-start;
+  align-items: center;
 }
 
 .user-info {
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   margin-top: 15px;
 }
 
 .header-login {
   display: flex;
+  align-items: center;
 }
 
 .logoImg {
-  margin-top: 3px;
+  margin-right: 10px;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
 }
 
 h1 {
   color: white;
   margin-left: 5px;
-  margin-right: 10px;
-  margin-top: 8px;
+  margin-right: 20px;
+  margin-top: 0;
 }
 
 h3 {
@@ -131,18 +165,42 @@ h3 {
 }
 
 a {
-  text-decoration-line: none;
-  margin-top: 15px;
-}
-
-.a {
-  text-decoration-line: none;
+  text-decoration: none;
   color: white;
   font-weight: 800;
   font-size: 16px;
 }
 
+.a {
+  text-decoration: none;
+  color: white;
+}
+
+a:hover, .a:hover, .header-sub h3:hover {
+  color: lightgray;
+}
+
 img {
+  width: 40px;
+  height: 40px;
+}
+
+.team-img {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  overflow: hidden;
+  margin-top: 20px;
+}
+
+.team-img img {
+  max-width: 100%;
+  max-height: 300px;
+  object-fit: contain;
+}
+
+.logoImg {
   width: 40px;
   height: 40px;
 }
